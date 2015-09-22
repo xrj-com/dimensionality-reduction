35583e49af2f1ead12df69c56dbd3ca06676c205
deallynomore <xianyiting@gmail.com>
2015-09-22 19:06:34 +0800

    edit

--- Parametric t-SNE/demo.m
+++ Parametric t-SNE/demo.m
@@ -4,13 +4,35 @@
     % Load MNIST dataset
     load 'mnist_train.mat'
     load 'mnist_test.mat'
-    
+    new_train_x = [];
+    new_train_labels = [];
+    new_test_x = [];
+    new_test_labels = [];
+    for label = 1:10
+        idx = find(train_labels==label);
+        len = length(idx);
+        %rand = randi([1, len],[1,round(len/10)]);
+        rand = randperm(len);
+        rand = rand(1:round(len/10));
+        new_train_x = [new_train_x; train_X(idx(rand),:)];
+        new_train_labels = [new_train_labels; train_labels(idx(rand),:)]; 
+    end
+    for label = 1:10
+        idx = find(test_labels==label);
+        len = length(idx);
+        %rand = randi([1, len],[1,round(len/10)]);
+        rand = randperm(len);
+        rand = rand(1:round(len/10));
+        new_test_x = [new_test_x; test_X(idx(rand),:)];
+        new_test_labels = [new_test_labels; test_labels(idx(rand),:)]; 
+    end
     % Set perplexity and network structure
     perplexity = 30;
     layers = [500 500 2000 2];
     
     % Train the parametric t-SNE network
-    [network, err] = train_par_tsne(train_X, train_labels, test_X, test_labels, layers, 'CD1');
+    %[network, err] = train_par_tsne(train_X, train_labels, test_X, test_labels, layers, 'CD1');
+    [network, err] = train_par_tsne(new_train_x, new_train_labels, new_test_x, new_test_labels, layers, 'CD1');
     
     % Construct training and test embeddings
     mapped_train_X = run_data_through_network(network, train_X);
